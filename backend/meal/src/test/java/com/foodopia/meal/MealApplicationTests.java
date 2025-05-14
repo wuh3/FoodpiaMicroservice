0afbd7b59bbeae4dbfac5626ec8540217d91c3d7
wuh3 <haozhew3@illinois.edu>
2025-05-13 22:31:38 -0500

    Initial commit

---
+++ backend/meal/src/test/java/com/foodopia/meal/MealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodopia.meal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
